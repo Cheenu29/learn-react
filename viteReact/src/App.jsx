9a4f07f2cf6f869f32ff46771df4f3d4ddf7651e
Rahul Katyal <rahulkatyal1997@gmail.com>
2025-01-09 17:11:01 +0530

    Tailwind CSS with props

--- viteReact/src/App.jsx
+++ viteReact/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Card from './components/Card'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -18,6 +19,12 @@ function App() {
 
   return (
     <>
+        <h1 className='bg-green-400 text-black p-4 rounded-xl mb-4'>Yo Yo</h1>
+
+        <Card mood = "hot"/>
+        <Card mood = "cold"/>          
+
+        <br/>
         <button onClick={Increment}>
           Add count is {count}
         </button>
